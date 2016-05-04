f54c658a224e59e04e38ed7ed6e104aef7499b18
derzorngottes <amy.conchie@gmail.com>
2016-05-03 18:36:30 -0700

    Refactor first three exercises

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var module1 = angular.
+var app = angular.module('angular-exercises', []);
+
+app.controller('helloController', ['$scope', function helloController($scope) {
+  $scope.greeting = 'Hello';
+}]);
+
+app.controller('whoController', ['$scope', function whoController($scope) {
+  $scope.name = '';
+}]);
+
+app.controller('dropdownController', ['$scope', function downdownController($scope) {
+  $scope.animals = [
+    'dogs',
+    'cats',
+    'dogs and cats',
+    'neither'
+  ];
+}])
